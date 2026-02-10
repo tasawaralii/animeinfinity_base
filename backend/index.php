cecb26612184d7b5413e6305090a83fef20d451b
Baby x Boss <speedodubbers@gmail.com>
2026-02-10 07:57:21 +0500

    Minor Fix

--- backend/index.php
+++ backend/index.php
@@ -15,8 +15,10 @@ require_once "./env.php";
 require_once "./src/lib/Router.php";
 require_once "./src/lib/Helper.php";
 require_once "./src/lib/Database.php";
+require_once "./src/controllers/anime.php";
 
 $router = new Router();
+$animeController = new AnimeController();
 
 $router->get("/", function () {
     Helper::successResponse("Working");
